ba520be469570e004918a2c8ede378ebcfe9008d
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-23 12:13:04 +0100

    ds

--- Home.py
+++ Home.py
@@ -271,7 +271,7 @@ if dois:
                     income_level_counts = df_authorships['incomeLevel'].value_counts().reset_index()
                     income_level_counts.columns = ['Income Level', 'Count']
                     income_level_counts
-                    fig2 = px.bar(income_level_counts, x='Income Level', y='Count', orientation='h', title='Income Level Counts')
+                    fig2 = px.bar(income_level_counts, y='Income Level', x='Count', orientation='h', title='Income Level Counts')
                     st.plotly_chart(fig2, use_container_width = True)
 
                 df_final    
