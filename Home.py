c5f4ad379e85da4a5fb77ef6b99daac1f2ae2a9b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-23 10:33:29 +0100

    d

--- Home.py
+++ Home.py
@@ -22,6 +22,7 @@ with st.sidebar:
 """
 )
 
+st.text_area()
 dois = ["10.1136/bmjgh-2023-013696", "10.1097/jac.0b013e31822cbdfd", '10.1080/02684527.2022.2055936', '10.1126/scitranslmed.aad9460']  # Add more DOIs as needed
 
 df_dois = pd.DataFrame(dois, columns=['doi'])
