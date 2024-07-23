aa64b5bff4b2a80dc303af2d23286b7c30ca1983
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-23 10:59:33 +0100

    sd

--- Home.py
+++ Home.py
@@ -39,7 +39,7 @@ if dois:
     submit = st.button('Calculate Citation Source Index')
 
     if submit:
-        with st.status("Finding sources and calculating CIS...", expanded=True) as status:
+        with st.status("Finding sources and calculating CSI...", expanded=True) as status:
             ## OPENALEX DATA RETRIEVAL
             def fetch_authorship_info_and_count(doi):
                 url = f"https://api.openalex.org/works/doi:{doi}"
@@ -208,7 +208,8 @@ if dois:
             citation_source_index = average_rank / country_count
             st.write(f'Citation Source Index: {round(citation_source_index, 2)}')
             df_final = df_authorships[['Citation Source Index', 'doi', 'title', 'all_authors', 'Countries']].drop_duplicates().reset_index(drop=True)
+            no_doi = df_final.nunique()
             df_final
-            status.update(label="Calculation complete!", state="complete", expanded=True)
+            status.update(label=f"Calculation complete! Results found for {no_doi} DOIs", state="complete", expanded=True)
 else:
     st.write("Enter DOIs in the text area to see the DataFrame.")
