714d0667e20b4035f790cfc844df313ac5813eaa
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-23 15:31:46 +0100

    ds

--- Home.py
+++ Home.py
@@ -387,9 +387,8 @@ if dois:
                 df_final  
 
                 source =   df_authorships['source'].value_counts().reset_index()
-                source
-                for index, row in source.iterrows():
-                    st.write(f"{row['count']} country affiliation found on {row['source']}.")
+                result_text = ", ".join([f"{row['count']} country affiliation found on {row['source']}" for index, row in source.iterrows()])
+                st.write(result_text)
 
                 status.update(label=f"Calculation complete! Results found for {no_doi_found} DOIs", state="complete", expanded=True)
 else:
