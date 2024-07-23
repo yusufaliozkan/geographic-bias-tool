f440d52bd9ca049371b2205dec13b9181d9713cc
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-23 10:47:11 +0100

    sd

--- Home.py
+++ Home.py
@@ -38,10 +38,7 @@ if dois:
 else:
     st.write("Enter DOIs in the text area to see the DataFrame.")
 
-dois = ["10.1136/bmjgh-2023-013696", "10.1097/jac.0b013e31822cbdfd", '10.1080/02684527.2022.2055936', '10.1126/scitranslmed.aad9460']  # Add more DOIs as needed
-
-df_dois = pd.DataFrame(dois, columns=['doi'])
-
+st.markdown('### Your DOIs are:')
 df_dois
 
 submit = st.button('Calculate Citation Source Index')
