8026dffed4a5e191903bbe7cca66031c1444062c
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-23 13:00:54 +0100

    ds

--- copyright.py
+++ copyright.py
@@ -10,7 +10,7 @@ def display_custom_license():
     f"""
     <br><a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons Licence" style="border-width:0" 
     src="https://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br />
-    © {current_year} Yusuf Ozkan. All rights reserved. This website is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
+    © {current_year} Imperial College London. All rights reserved. This tool is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
     </br>
     <br>
     <strong>Cite this page:</strong> Imperial College London ‘<em>Geographic Bias Tool</em>’, Created July 2024, Accessed {cite_today}. <a href="https://geographic-bias-tool.streamlit.app/">https://geographic-bias-tool.streamlit.app/</a>.
