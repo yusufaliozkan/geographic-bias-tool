67d61d92eae4c613d8bd2f3e0b04dc29fa858c7e
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-25 11:50:02 +0100

    ds

--- Home.py
+++ Home.py
@@ -436,7 +436,7 @@ else:
                                 country_counts.columns = ['Country Name', 'Count']
                                 fig = px.bar(country_counts, x='Country Name', y='Count', title='Country Counts')
                                 col1.plotly_chart(fig, use_container_width = True)
-                                country_counts = pd.merge(country_counts, df_result, left_on='Country Name', right_on='name')
+                                country_counts = pd.merge(country_counts, df_result, on='Country Name')
                                 country_counts = country_counts.drop(columns=['Unnamed: 0', 'Country Code 3', 'Country Code 2', 'name'])
                                 country_counts
                             with col1:
