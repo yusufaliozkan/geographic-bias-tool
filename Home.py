d1af67fccef927025b4bf9d36b88bf43072aa29c
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-25 11:36:40 +0100

    sd

--- Home.py
+++ Home.py
@@ -436,7 +436,8 @@ else:
                                 country_counts.columns = ['Country Name', 'Count']
                                 fig = px.bar(country_counts, x='Country Name', y='Count', title='Country Counts')
                                 col1.plotly_chart(fig, use_container_width = True)
-                                country_counts
+                                # country_counts = pd.merge(country_counts, )
+                                df_countries
                             with col1:
                                 income_level_counts = df_authorships['incomeLevel'].value_counts().reset_index()
                                 income_level_counts.columns = ['Income Level', 'Count']
