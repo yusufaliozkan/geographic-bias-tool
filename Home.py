2dfdf94e8e6bd87498c3c33d282e2f1f88d0a54d
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-25 11:08:43 +0100

    sd

--- Home.py
+++ Home.py
@@ -30,9 +30,9 @@ with st.sidebar:
     with st.expander('Licence'):  
         display_custom_license()
     with st.expander('Source code'):
-        st.write('Source code and datasets used for this tool are available here: [Geographic Bias Tool GitHub repo](https://github.com/yusufaliozkan/geographic-bias-tool/blob/main/Home.py)')
+        st.write('Source code and datasets used for this tool are available here:')
         st.caption(
-            "[![GitHubs repo](https://img.shields.io/badge/GitHub-repo-0a507a?logo=github)](https://github.com/yusufaliozkan/geographic-bias-tool) "
+            "[![GitHub repo](https://img.shields.io/badge/GitHub-repo-0a507a?logo=github)](https://github.com/yusufaliozkan/geographic-bias-tool) "
         )
     with st.expander('Contact'):
         st.write('For your questions, you can contact [Yusuf Ozkan, Research Outputs Analyst](https://profiles.imperial.ac.uk/y.ozkan) at Imperial College London.')
