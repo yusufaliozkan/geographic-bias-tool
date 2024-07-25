621402034484c6105279c004c7be5d303e1726e1
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-25 12:03:44 +0100

    sd

--- Home.py
+++ Home.py
@@ -469,8 +469,11 @@ else:
                     country_counts = country_counts.drop(columns=['Unnamed: 0', 'Country Code 3', 'Country Code 2', 'name', 'Year','GNI'])
                     columns = ['Country Name', 'Rank', 'incomeLevel', 'Count']
                     country_counts = country_counts[columns]
-                    # columns = ['Country Name', 'Rank', 'Income Level', 'Author Count']
-                    country_counts = country_counts[columns]
+                    new_column_names = {
+                        'incomeLevel': 'Income Level',
+                        'Count': 'Author Count',
+                    }
+                    country_counts = country_counts.rename(columns=new_column_names)
                     country_counts = country_counts.sort_values(by='Rank', ascending=True).reset_index(drop=True)
                     country_counts
 
