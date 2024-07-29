8ca14c6cce80e8b629adf92354832c46fbe1b3ab
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-29 16:28:55 +0100

    sd

--- Home.py
+++ Home.py
@@ -464,6 +464,7 @@ else:
                                 col2.plotly_chart(fig2, use_container_width = True)
                             col1, col2 = st.columns([3,1])
                             with col1:
+                                citation_source_index
                                 fig = px.choropleth(
                                     country_counts,
                                     locations='Country Name',
@@ -473,7 +474,6 @@ else:
                                     color_continuous_scale='Viridis',
                                     title='Author Affiliations on Map'
                                 )
-                                fig
                                 fig.update_layout(
                                     width=1200,  # Set the width as per your requirement
                                     height=700   # Set the height as per your requirement
