6da320f3d5d8b49167b377ea49259c2a858d5c2e
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-29 16:43:17 +0100

    sd

--- Home.py
+++ Home.py
@@ -236,15 +236,15 @@ else:
                         return title, authorship_info, author_count
                     else:
                         return '', [], 0
-
-                # Function to fetch author details using author ID
-                def fetch_author_details(author_id):
-                    response = requests.get(author_id)
-                    if response.status_code == 200:
-                        data = response.json()
-                        return data
-                    else:
-                        return None
+                if not exclude_author_profile_page:
+                    # Function to fetch author details using author ID
+                    def fetch_author_details(author_id):
+                        response = requests.get(author_id)
+                        if response.status_code == 200:
+                            data = response.json()
+                            return data
+                        else:
+                            return None
 
                 # Fetch authorship information for each DOI and store it in a new DataFrame
                 authorship_data = []
