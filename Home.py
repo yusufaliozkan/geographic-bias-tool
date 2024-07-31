e2577b3956ef9a3696694bbe802768f4a1be0d24
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-31 14:11:52 +0100

    sd

--- Home.py
+++ Home.py
@@ -18,11 +18,8 @@ st.set_page_config(layout = "wide",
 pd.set_option('display.max_colwidth', None)
 
 st.logo(
-    image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtoX76TyVQs-o1vEvNuAnYX0zahtSui173gg&s',
-    icon_image="""<a href="https://www.imperial.ac.uk">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/0/06/Imperial_College_London_new_logo.png" width="150">
-        </a>
-        """
+    image='https://upload.wikimedia.org/wikipedia/commons/0/06/Imperial_College_London_new_logo.png',
+    icon_image='https://upload.wikimedia.org/wikipedia/commons/0/06/Imperial_College_London_new_logo.png'
     )
 
 with st.sidebar:
