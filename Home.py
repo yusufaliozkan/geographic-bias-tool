70204e0f8316c20a0be7c2ca33a23747540dc74b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-31 13:08:17 +0100

    ds

--- Home.py
+++ Home.py
@@ -11,16 +11,16 @@ import numpy as np
 import plotly.express as px
 import time
 
+testing_page = st.Page('testing.py', title='Testing page')
+pg = st.navigation([testing_page])
+st.set_page_config(page_title="Data manager")
+pg.run()
+
 st.set_page_config(layout = "wide", 
                     page_title='Geographic Bias Tool',
                     page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtoX76TyVQs-o1vEvNuAnYX0zahtSui173gg&s",
                     initial_sidebar_state="auto") 
 pd.set_option('display.max_colwidth', None)
-
-testing_page = st.Page('testing.py', title='Testing page')
-pg = st.navigation([testing_page])
-st.set_page_config(page_title="Data manager")
-pg.run()
 
 
 with st.sidebar:
