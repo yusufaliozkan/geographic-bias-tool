7af535f512a9b3c2f6f6b378e51f238a21a93b82
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-31 13:26:23 +0100

    sd

--- Home.py
+++ Home.py
@@ -11,16 +11,18 @@ import numpy as np
 import plotly.express as px
 import time
 
-# testing_page = st.Page('testing.py', title='Testing page')
-# pg = st.navigation([testing_page])
-# pg.run()
+home_page = st.Page("Home.py", title="Home", icon=":house:")
+testing_page = st.Page("testing.py", title="Testing", icon=":microscope:")
 
-st.set_page_config(layout = "wide", 
-                    page_title='Geographic Bias Tool',
-                    page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtoX76TyVQs-o1vEvNuAnYX0zahtSui173gg&s",
-                    initial_sidebar_state="auto") 
-pd.set_option('display.max_colwidth', None)
+# Set up navigation
+pg = st.navigation([home_page, testing_page])
 
+# Set page config (if needed)
+st.set_page_config(page_title="Geographic Bias Tool", 
+                   page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtoX76TyVQs-o1vEvNuAnYX0zahtSui173gg&s")
+
+# Run the selected page
+pg.run()
 
 with st.sidebar:
     st.markdown(
