81d537737c4124fc105d3b705877f24c74dce6ee
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-31 13:27:06 +0100

    sd

--- testing.py
+++ testing.py
@@ -11,10 +11,5 @@ import numpy as np
 import plotly.express as px
 import time
 
-st.set_page_config(layout = "wide", 
-                    page_title='Geographic Bias Tool',
-                    page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtoX76TyVQs-o1vEvNuAnYX0zahtSui173gg&s",
-                    initial_sidebar_state="auto") 
-pd.set_option('display.max_colwidth', None)
 
 st.write('Testing page')
